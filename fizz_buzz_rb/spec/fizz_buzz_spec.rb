4468d66e0c88edd1d67b3330135c1e383454933c
Fraseer <fraseer1430@gmail.com>
2021-10-20 11:20:11 +0200

    Creates rb spec file

---
+++ fizz_buzz_rb/spec/fizz_buzz_spec.rb
@@ -0,0 +1,19 @@
+describe FizzBuzz do
+  subject { FizzBuzz.new }
+
+  it 'is expected to return a integer if non of the game rules apply' do
+    expect(subject.check(1).class).to eq(Integer)
+  end
+
+  it 'is expected that 3 will return fizz' do
+    expect(subject.check3).to eq 'fizz'
+  end
+
+  it 'is expected that 5 will return buzz' do
+    expect(subject.check5).to eq 'buzz'
+  end
+
+  it 'is expected that 15 will return fizz-buzz' do
+    expect(subject.check15).to eq 'fizz-buzz'
+  end
+end
