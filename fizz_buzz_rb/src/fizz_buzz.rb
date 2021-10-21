14cb0386949f20b0d07bb8e867e543a477416187
Fraseer <fraseer1430@gmail.com>
2021-10-21 11:36:43 +0200

    Solves for both js & rb

--- fizz_buzz_rb/src/fizz_buzz.rb
+++ fizz_buzz_rb/src/fizz_buzz.rb
@@ -1,6 +1,6 @@
 class FizzBuzz
 
   def check(value)
-    return value%15 == 0 ? "Fizz-Buzz" : value%5 == 0 ? "Buzz" : value%3 == 0 ? "Fizz" : value
+    value%15 == 0 ? "Fizz-Buzz" : value%5 == 0 ? "Buzz" : value%3 == 0 ? "Fizz" : value
   end
 end
